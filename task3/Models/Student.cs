5276a8b30bbcc7a26f484853e5a66fe837900936
khaledali9999 <elsaadanykhaled5@gmail.com>
2025-09-16 19:57:32 +0300

    Student.cs

--- task3/Models/Student.cs
+++ task3/Models/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace task3.Models
 {
-    internal class Student
+    public class Student
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
